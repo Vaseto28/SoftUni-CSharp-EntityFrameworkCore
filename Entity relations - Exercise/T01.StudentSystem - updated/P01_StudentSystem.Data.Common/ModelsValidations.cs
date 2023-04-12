42fbc0f72f7842ba9d37c2d7b324069a922ca08e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-12 18:19:11 +0300

    Completed task: Created ModelValidations class

---
+++ Entity relations - Exercise/T01.StudentSystem - updated/P01_StudentSystem.Data.Common/ModelsValidations.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_StudentSystem.Data.Common;
+
+public static class ModelsValidations
+{
+    //Student
+    public const int StudentNameMaxLength = 100;
+    public const int StudentPhoneNumberLength = 10;
+
+    //Course
+    public const int CourseNameMaxLength = 30;
+
+    //Resource
+    public const int ResourceNameMaxLength = 30;
+}
+
