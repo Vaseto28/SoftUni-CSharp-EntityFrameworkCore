0987208f0b23e2082f761ecc4397e2b4961b8aed
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-16 14:12:34 +0300

    Completed task: Created Student model

---
+++ Entity relations - Exercise/T01.StudentSystem - updated/P01_StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using P01_StudentSystem.Data.Common;
+
+namespace P01_StudentSystem.Data.Models;
+
+public class Student
+{
+    public Student()
+    {
+        this.Homeworks = new HashSet<Homework>();
+        this.StudentCourses = new HashSet<StudentCourse>();
+    }
+
+    [Key]
+    public int StudentId { get; set; }
+
+    [Required]
+    [MaxLength(ModelsValidations.StudentNameMaxLength)]
+    public string Name { get; set; }
+
+    [MaxLength(ModelsValidations.StudentPhoneNumberLength)]
+    public string PhoneNumber { get; set; }
+
+    public DateTime RegisteredOn { get; set; }
+
+    public DateTime Birthday { get; set; }
+
+    public virtual ICollection<Homework> Homeworks { get; set; }
+
+    public virtual ICollection<StudentCourse> StudentCourses { get; set; }
+}
