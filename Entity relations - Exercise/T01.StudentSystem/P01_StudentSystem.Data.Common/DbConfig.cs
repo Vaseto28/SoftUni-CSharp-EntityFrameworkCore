c5f184b0d1ee440ef4bd31048b6505b7aefab02e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-16 14:13:52 +0300

    Completed task: Created DbConfig class

---
+++ Entity relations - Exercise/T01.StudentSystem/P01_StudentSystem.Data.Common/DbConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace P01_StudentSystem.Data.Common;
+
+public static class DbConfig
+{
+    public const string ConnectionString = @"Server=.,1433;Initial Catalog=StudentSystem;Persist Security Info=False;User ID=sa;Password=28120601V;TrustServerCertificate=True;Encrypt=false";
+}
