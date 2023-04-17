7df8e82a9a338e82971d32775699f30ab3d566b1
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-17 21:54:22 +0300

    Completed task: Created Homework model

---
+++ Entity relations - Exercise/T01.StudentSystem/P01_StudentSystem.Data.Models/Homework.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using P01_StudentSystem.Data.Models.Enumerators;
+
+namespace P01_StudentSystem.Data.Models;
+
+public class Homework
+{
+	[Key]
+	public int HomeworkId { get; set; }
+
+	[Required]
+	public string Content { get; set; } = null!;
+
+	public ContentTypeEnum ContentType { get; set; }
+
+	public DateTime SubmissionTime { get; set; }
+
+	[ForeignKey(nameof(Student))]
+	public int StudentId { get; set; }
+
+	public Student Student { get; set; } = null!;
+
+	[ForeignKey(nameof(Course))]
+	public int CourseId { get; set; }
+
+	public Course Course { get; set; } = null!;
+}
+
