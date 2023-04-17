422f1a602dff119bd9054e7f9e1d9a8bf988779b
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-17 21:54:40 +0300

    Completed task: Created Resource model

---
+++ Entity relations - Exercise/T01.StudentSystem/P01_StudentSystem.Data.Models/Resource.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using P01_StudentSystem.Data.Common;
+using P01_StudentSystem.Data.Models.Enumerators;
+
+namespace P01_StudentSystem.Data.Models;
+
+public class Resource
+{
+	[Key]
+	public int ResourceId { get; set; }
+
+	[MaxLength(ModelsValidations.ResourceNameMaxLength)]
+	[Required]
+	public string Name { get; set; } = null!;
+
+	public string Url { get; set; }
+
+	public ResourceTypeEnum ResourceType { get; set; }
+
+	[ForeignKey(nameof(Course))]
+	public int CourseId { get; set; }
+
+	public Course Course { get; set; }
+}
+
