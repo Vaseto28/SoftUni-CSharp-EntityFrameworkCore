c366479a02986385f0d71860abcd6a17ccc26b99
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-18 23:26:20 +0300

    Completed task; Created Country model

---
+++ Entity relations - Exercise/T02.FootballBetting - sln/P02.FootballBetting/P02_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using P02_FootballBetting.Common;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Country
+{
+	public Country()
+	{
+		this.Towns = new HashSet<Town>();
+	}
+
+	[Key]
+	public int CountryId { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.CountryNameMaxLength)]
+	public string Name { get; set; }
+
+	public virtual ICollection<Town> Towns { get; set; }
+}
+
