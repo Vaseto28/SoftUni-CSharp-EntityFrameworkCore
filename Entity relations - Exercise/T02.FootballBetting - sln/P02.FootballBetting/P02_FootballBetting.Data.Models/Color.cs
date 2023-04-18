4479b2ba175dc0ed27a31edbb3439dcaf254db7e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-18 23:26:09 +0300

    Completed task: Created Colour model

---
+++ Entity relations - Exercise/T02.FootballBetting - sln/P02.FootballBetting/P02_FootballBetting.Data.Models/Color.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using P02_FootballBetting.Common;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Color
+{
+	public Color()
+	{
+		this.PrimaryKitTeams = new HashSet<Team>();
+		this.SecondaryKitTeams = new HashSet<Team>();
+    }
+
+	[Key]
+	public int ColorId { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.ColorNameMaxLength)]
+	public string Name { get; set; }
+
+	public virtual ICollection<Team> PrimaryKitTeams { get; set; }
+
+	public virtual ICollection<Team> SecondaryKitTeams { get; set; }
+}
+
