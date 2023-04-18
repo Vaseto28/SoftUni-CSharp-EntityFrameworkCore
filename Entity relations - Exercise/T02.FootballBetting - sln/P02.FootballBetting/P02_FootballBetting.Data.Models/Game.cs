e4ef78d45befdf906933b219db0cd6002a0d52d7
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-18 23:27:04 +0300

    Completed task: Created Game model

---
+++ Entity relations - Exercise/T02.FootballBetting - sln/P02.FootballBetting/P02_FootballBetting.Data.Models/Game.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using P02_FootballBetting.Common;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Game
+{
+	public Game()
+	{
+		this.PlayersStatistics = new HashSet<PlayerStatistic>();
+		this.Bets = new HashSet<Bet>();
+    }
+
+	[Key]
+	public int GameId { get; set; }
+
+	public int HomeTeamId { get; set; }
+
+	[Required]
+	public virtual Team HomeTeam { get; set; }
+
+	public int AwayTeamId { get; set; }
+
+	[Required]
+	public virtual Team AwayTeam { get; set; }
+
+	public byte HomeTeamGoals { get; set; }
+
+	public byte AwayTeamGoals { get; set; }
+
+	public DateTime DateTime { get; set; }
+
+	public double HomeTeamBetRate { get; set; }
+
+	public double AwayTeamBetRate { get; set; }
+
+	public double DrawBetRate { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.GameResultMaxLength)]
+	public string Result { get; set; }
+
+	public virtual ICollection<PlayerStatistic> PlayersStatistics { get; set; }
+
+	public virtual ICollection<Bet> Bets { get; set; }
+}
+
