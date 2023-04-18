24bd1d636434873a46d98584258f0638bf876d33
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-18 23:25:15 +0300

    Completed task: Created Config class

---
+++ Entity relations - Exercise/T02.FootballBetting - sln/P02.FootballBetting/P02_FootballBetting.Common/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace P02_FootballBetting.Common;
+
+public static class Config
+{
+    public const string ConnectionString = @"Server=.,1433;Initial Catalog=FootballBetting;Persist Security Info=False;User ID=sa;Password=28120601V;TrustServerCertificate=True;";
+}
+
