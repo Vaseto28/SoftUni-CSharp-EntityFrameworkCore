4feb8e9b4ef18762cd1168de52021fcd464c53f6
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-19 09:05:15 +0300

    Completed task: Created Position model

---
+++ Entity relations - Exercise/T02.FootballBetting - sln/P02.FootballBetting/P02_FootballBetting.Data.Models/Position.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using P02_FootballBetting.Common;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Position
+{
+	public Position()
+	{
+		this.Players = new HashSet<Player>();
+	}
+
+	[Key]
+	public int PositionId { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.PositionNameMaxLength)]
+	public string Name { get; set; }
+
+	public virtual ICollection<Player> Players { get; set; }
+}
+
