43b0d8c20b336bf264f7bc2012182b75b5093d75
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-19 09:05:50 +0300

    Completed task: Created User model

---
+++ Entity relations - Exercise/T02.FootballBetting - sln/P02.FootballBetting/P02_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using P02_FootballBetting.Common;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class User
+{
+	public User()
+	{
+		this.Bets = new HashSet<Bet>();
+	}
+
+	[Key]
+	public int UserId { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.UserUsernameMaxLength)]
+	public string Username { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.UserPasswordMaxLength)]
+	public string Password { get; set; }
+
+	[Required]
+	public string Email { get; set; }
+
+	[Required]
+	[MaxLength(ValidationParams.UserNameMaxLength)]
+	public string Name { get; set; }
+
+	public decimal Balance { get; set; }
+
+	public virtual ICollection<Bet> Bets { get; set; }
+}
+
