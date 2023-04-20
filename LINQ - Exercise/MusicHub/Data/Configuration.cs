cd1cdbd6541f2f3a052a4a6fee9770e038158693
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-20 11:12:29 +0300

    Completed task: Created Config class

---
+++ LINQ - Exercise/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=.,1433;Database=MusicHub;Trusted_Connection=True;User ID=sa;Password=28120601V;integrated security=False;";
+    }
+}
