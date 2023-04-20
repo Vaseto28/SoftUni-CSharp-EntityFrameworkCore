8d14c1a673dbad8062373e7e39f92b07deb9ce53
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-20 11:13:01 +0300

    Completed task: Created Performer model

---
+++ LINQ - Exercise/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models;
+
+public class Performer
+{
+	public Performer()
+	{
+		this.PerformerSongs = new HashSet<SongPerformer>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.PerformerFirstNameMaxLength)]
+	public string FirstName { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.PerformerLastNameMaxLength)]
+	public string LastName { get; set; }
+
+	[Required]
+	public int Age { get; set; }
+
+	[Required]
+	public decimal NetWorth { get; set; }
+
+	public virtual ICollection<SongPerformer> PerformerSongs { get; set; }
+}
+
