0b091dc4be2471765cbe1cab29482a6df1055271
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-20 11:12:41 +0300

    Completed task: Created Album model

---
+++ LINQ - Exercise/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models;
+
+public class Album
+{
+	public Album()
+	{
+		this.Songs = new HashSet<Song>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.AlbumNameMaxLength)]
+	public string Name { get; set; }
+
+	[Required]
+	public DateTime ReleaseDate { get; set; }
+
+	public decimal Price => this.Songs.Sum(x => x.Price);
+
+	[ForeignKey(nameof(Producer))]
+	public int? ProducerId { get; set; }
+
+	public virtual Producer Producer { get; set; }
+
+	public virtual ICollection<Song> Songs { get; set; }
+}
+
