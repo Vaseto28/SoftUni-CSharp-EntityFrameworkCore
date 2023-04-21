184989c5a76f368c86010360969b57a9f34ab6ff
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-21 13:02:25 +0300

    Completed task: Created Writer model

---
+++ LINQ - Exercise/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models;
+
+public class Writer
+{
+	public Writer()
+	{
+		this.Songs = new HashSet<Song>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.WriterNameMaxLength)]
+	public string Name { get; set; }
+
+	public string? Pseudonym { get; set; }
+
+	public virtual ICollection<Song> Songs { get; set; }
+}
+
