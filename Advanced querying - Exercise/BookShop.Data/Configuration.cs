5a8de8d24d947419f07be60828eecc9a47ad4dc7
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-22 16:39:03 +0300

    Completed task: Created Configuration class

---
+++ Advanced querying - Exercise/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=.,1433;Initial Catalog=BookShop;User ID=sa;Password=28120601V;TrustServerCertificate=True;";
+    }
+}
