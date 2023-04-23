891ac6ab1753b0042a73e09ac81cd1906d9e4db9
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-23 22:34:27 +0300

    Completed task: Created Category model

---
+++ Advanced querying - Exercise/BookShop.Models/Category.cs
@@ -0,0 +1,22 @@
+﻿namespace BookShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using BookShop.Models.Validatons;
+
+    public class Category
+    {
+        public Category()
+        {
+            this.CategoryBooks = new HashSet<BookCategory>();
+        }
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [MaxLength(ValidationConstraints.CategoryNameMaxLength)]
+        public string Name { get; set; }
+
+        public virtual ICollection<BookCategory> CategoryBooks { get; set; }
+    }
+}
