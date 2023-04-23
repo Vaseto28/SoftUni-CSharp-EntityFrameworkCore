8ee17a1c220529b4e8875e06e6f7019960fdc925
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-23 22:33:35 +0300

    Completed task: Created Author model

---
+++ Advanced querying - Exercise/BookShop.Models/Author.cs
@@ -0,0 +1,25 @@
+﻿namespace BookShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using BookShop.Models.Validatons;
+
+    public class Author
+    {
+        public Author()
+        {
+            this.Books = new HashSet<Book>();
+        }
+
+        [Key]
+        public int AuthorId { get; set; }
+
+        [MaxLength(ValidationConstraints.AuthorFirstNameMaxLength)]
+        public string? FirstName { get; set; }
+
+        [MaxLength(ValidationConstraints.AuthorLastNameMaxLength)]
+        public string LastName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
