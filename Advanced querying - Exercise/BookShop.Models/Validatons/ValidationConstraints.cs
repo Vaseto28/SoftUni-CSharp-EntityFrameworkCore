337f17f2915c6f501a796d5e712d00bff85f394d
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-24 09:31:56 +0300

    Completed task: Created ValidationConstraints class

---
+++ Advanced querying - Exercise/BookShop.Models/Validatons/ValidationConstraints.cs
@@ -0,0 +1,16 @@
+﻿namespace BookShop.Models.Validatons;
+
+public static class ValidationConstraints
+{
+	// Author
+	public const int AuthorFirstNameMaxLength = 50;
+	public const int AuthorLastNameMaxLength = 50;
+
+	// Book
+	public const int BookTitleMaxLength = 50;
+	public const int BookDescriptionMaxLength = 1000;
+
+	// Category
+	public const int CategoryNameMaxLength = 50;
+}
+
