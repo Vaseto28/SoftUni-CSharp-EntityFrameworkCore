fe04932fa4e38af1fe41f1796eceb9e1df203085
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-27 09:53:21 +0300

    Completed task: Created ExportCategoriesDTO class

---
+++ JSON processing - Exercise/T01.ProductShopTask/ProductShop/DTOs/Export/ExportCategoriesDTO.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Export;
+
+public class ExportCategoriesDTO
+{
+	[JsonProperty("category")]
+	public string Category { get; set; } = null!;
+
+	[JsonProperty("productsCount")]
+	public int ProductsCount { get; set; }
+
+	[JsonProperty("averagePrice")]
+	public decimal AveragePrice { get; set; }
+
+	[JsonProperty("totalRevenue")]
+	public decimal TotalRevenue { get; set; }
+}
+
