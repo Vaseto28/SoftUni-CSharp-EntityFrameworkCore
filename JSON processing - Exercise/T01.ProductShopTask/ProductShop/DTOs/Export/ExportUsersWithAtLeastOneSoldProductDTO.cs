900d5ecf8134bbf150ad16863f347c352a79b054
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-27 09:54:50 +0300

    Completed task: Created ExportUsersWithAtLeastOneSoldProductDTO class

---
+++ JSON processing - Exercise/T01.ProductShopTask/ProductShop/DTOs/Export/ExportUsersWithAtLeastOneSoldProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Export;
+
+public class ExportUsersWithAtLeastOneSoldProductDTO
+{
+	[JsonProperty("firstName")]
+	public string? FirstName { get; set; }
+
+	[JsonProperty("lastName")]
+	public string LastName { get; set; } = null!;
+
+	[JsonProperty("soldProducts")]
+	public ICollection<ExportProductInformationDTO>? Products { get; set; }
+}
+
