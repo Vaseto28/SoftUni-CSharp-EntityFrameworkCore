4b406532acd066ebbf5f6b94a3df291c1658930a
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-04-27 10:02:47 +0300

    Completed task: Created ImportCategoriesProductsDTO class

---
+++ JSON processing - Exercise/T01.ProductShopTask/ProductShop/DTOs/Import/ImportCategoriesProductsDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Import;
+
+public class ImportCategoriesProductsDTO
+{
+	[JsonProperty("CategoryId")]
+	public int CategoryId { get; set; }
+
+	[JsonProperty("ProductId")]
+	public int ProductId { get; set; }
+}
+
