e373fe440c69d4dc56a558f0b2f3b7b6456fb47c
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-02 08:58:47 +0300

    Completed task: Created Category model

---
+++ JSON processing - Exercise/T01.ProductShopTask/ProductShop/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductShop.Models;
+
+using System.Collections.Generic;
+
+public class Category
+{
+    public Category()
+    {
+        this.CategoriesProducts = new List<CategoryProduct>();
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<CategoryProduct> CategoriesProducts { get; set; }
+}
