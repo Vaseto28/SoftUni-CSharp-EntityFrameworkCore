e4e33711294a0dce4c5ba9d68d278ed712d7ab36
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-02 09:01:13 +0300

    Completed task: Created User model

---
+++ JSON processing - Exercise/T01.ProductShopTask/ProductShop/Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace ProductShop.Models;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class User
+{
+    public User()
+    {
+        this.ProductsSold = new List<Product>();
+        this.ProductsBought = new List<Product>();
+    }
+
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public int? Age { get; set; }
+
+    [InverseProperty("Seller")]
+    public virtual ICollection<Product> ProductsSold { get; set; }
+
+    [InverseProperty("Buyer")]
+    public virtual ICollection<Product> ProductsBought { get; set; }
+}
