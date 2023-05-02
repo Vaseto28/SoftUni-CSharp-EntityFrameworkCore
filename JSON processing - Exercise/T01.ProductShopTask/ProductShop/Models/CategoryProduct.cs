932c05bb015cfa19f76d2e58c3ec63d1ae384011
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-02 08:59:26 +0300

    Completed task: Created CategoryProduct category

---
+++ JSON processing - Exercise/T01.ProductShopTask/ProductShop/Models/CategoryProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductShop.Models;
+
+public class CategoryProduct
+{
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; } = null!;
+
+    public int ProductId { get; set; }
+    public virtual Product Product { get; set; } = null!;
+}
