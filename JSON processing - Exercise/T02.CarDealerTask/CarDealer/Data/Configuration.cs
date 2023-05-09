3cfb2295c7e12e37e9bbc3a1a921685b91099c00
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-09 08:58:06 +0300

    Completed task: Created Configuration class

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/Data/Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace CarDealer.Data;
+
+public static class Configuration
+{
+    public const string ConnectionString = @"Server=.,1433;Initial Catalog=CarDealer;User ID=sa;Password=28120601V;Trusted_Connection=True;Integrated security=false";
+}
