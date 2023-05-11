98cbef7bcb9faaf25f65cfa864b5663b31a25397
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-11 11:15:53 +0300

    Completed task: Created ImportCarDTO class

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/DTOs/Import/ImportCarDTO.cs
@@ -0,0 +1,24 @@
+﻿using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Import;
+
+public class ImportCarDTO
+{
+    public ImportCarDTO()
+    {
+        this.PartsCars = new List<PartCar>();
+    }
+
+    [JsonProperty("make")]
+    public string Make { get; set; } = null!;
+
+    [JsonProperty("model")]
+    public string Model { get; set; } = null!;
+
+    [JsonProperty("traveledDistance")]
+    public long TravelledDistance { get; set; }
+
+    [JsonProperty("partsId")]
+    public ICollection<PartCar> PartsCars { get; set; }
+}
