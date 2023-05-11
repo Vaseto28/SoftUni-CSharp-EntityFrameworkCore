81b3f72d5b011b3e930a1603b0d083b21caed3a9
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-11 11:13:24 +0300

    Completed task: Created ExportCarsDTO class

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/DTOs/Export/ExportCarsDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Export;
+
+public class ExportCarsDTO
+{
+	[JsonProperty("Make")]
+	public string Make { get; set; } = null!;
+
+	[JsonProperty("Model")]
+	public string Model { get; set; } = null!;
+
+	[JsonProperty("TraveledDistance")]
+	public long TraveledDistance { get; set; }
+}
+
