e816d4840952c1a6389148b9bffca71f32c622dd
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-14 16:29:29 +0300

    Completed task: Created Car model

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer.Models;
+
+public class Car
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Make { get; set; } = null!;
+
+    public string Model { get; set; } = null!;
+
+    public long TraveledDistance { get; set; }
+
+    public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>();    
+
+    public virtual ICollection<PartCar> PartsCars { get; set; } = new List<PartCar>();
+}
