e02b4701b3bff10a8fb0c449df88d2aefaf782b0
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-14 16:28:25 +0300

    Completed task: Created ImportCustomerDTO class

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/DTOs/Import/ImportCustomersDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Import;
+
+public class ImportCustomersDTO
+{
+	[JsonProperty("name")]
+	public string Name { get; set; }
+
+	[JsonProperty("birthDate")]
+	public DateTime BirthDate { get; set; }
+
+	[JsonProperty("isYoungDriver")]
+	public bool IsYoungDriver { get; set; }
+}
+
