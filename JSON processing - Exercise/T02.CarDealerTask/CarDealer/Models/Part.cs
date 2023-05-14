1026b1ed18971dd7ec4d8b24856c872f8eb711c4
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-14 16:29:52 +0300

    Completed task: Created Part model

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/Models/Part.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models;
+
+public class Part
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!; 
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    [ForeignKey(nameof(Supplier))]
+    public int SupplierId { get; set; }
+
+    public virtual Supplier Supplier { get; set; } = null!;
+
+    public virtual ICollection<PartCar> PartsCars { get; set; } = new List<PartCar>();
+}
