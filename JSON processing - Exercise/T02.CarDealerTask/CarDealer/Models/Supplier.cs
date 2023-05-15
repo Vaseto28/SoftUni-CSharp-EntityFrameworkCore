e78f6e24b38ddbb70427b06e79222f89051ad4e0
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-15 08:10:59 +0300

    Completed task: Created Supplier model

---
+++ JSON processing - Exercise/T02.CarDealerTask/CarDealer/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer.Models;
+
+public class Supplier
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool IsImporter { get; set; }
+
+    public virtual ICollection<Part> Parts { get; set; } = new List<Part>();
+}
