63f95e49abff72757efc7bd53f49fa86c3f33a1b
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-16 11:13:17 +0300

    Completed task: Created Config class

---
+++ XML processing - Exercise/T01.ProductShopTask/ProductShop/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=.;Initial Catalog=ProductShop;Persist Security Info=False;User ID=sa;Password=28120601V;TrustServerCertificate=True;;Encrypt=False";
+    }
+}
