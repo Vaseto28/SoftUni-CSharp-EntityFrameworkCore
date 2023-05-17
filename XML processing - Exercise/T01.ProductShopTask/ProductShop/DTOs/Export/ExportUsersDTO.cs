06e6f8f9b7129d6ed0cdf54a927258f4ef73cac0
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-17 22:56:16 +0300

    Completed task: Created ExportUsersDTO class

---
+++ XML processing - Exercise/T01.ProductShopTask/ProductShop/DTOs/Export/ExportUsersDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Export;
+
+[XmlType("User")]
+public class ExportUsersDTO
+{
+	[XmlElement("firstName")]
+	public string FirstName { get; set; } = null!;
+
+	[XmlElement("lastName")]
+	public string LastName { get; set; } = null!;
+
+	[XmlElement("soldProducts")]
+	public ICollection<ExportSoldProductsDTO> Products { get; set; } = null!;
+}
+
