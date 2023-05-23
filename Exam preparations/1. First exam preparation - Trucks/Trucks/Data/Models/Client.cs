132758924676435f9b463657413a64c63dc4b3bb
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-23 20:55:29 +0300

    Completed task: Created Client model

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/Data/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trucks.Utilities;
+
+namespace Trucks.Data.Models;
+
+public class Client
+{
+	public Client()
+	{
+		this.ClientsTrucks = new HashSet<ClientTruck>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.ClientNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[MaxLength(ValidationConstraints.ClientNationalityMaxLength)]
+	public string Nationality { get; set; } = null!;
+
+	[Required]
+	public string Type { get; set; } = null!;
+
+	public virtual ICollection<ClientTruck> ClientsTrucks  { get; set; }
+}
+
