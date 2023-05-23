95b9074c44c12a2a72143ac31363a93d8ff3c7ba
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-23 20:55:16 +0300

    Completed task: Created Configuration class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Trucks.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=Trucks;User ID=sa;Password=28120601V;Trusted_Connection=True;TrustServerCertificate=True;Integrated security=false";
+    }
+}
