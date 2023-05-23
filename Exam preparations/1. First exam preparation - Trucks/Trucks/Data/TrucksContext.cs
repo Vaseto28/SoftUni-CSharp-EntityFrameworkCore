a5e89320d7e9f33b472befe474491fc3a66dc30e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-23 20:57:11 +0300

    Completed task: Created DbContext class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/Data/TrucksContext.cs
@@ -0,0 +1,41 @@
+﻿namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Trucks.Data.Models;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext()
+        { 
+        }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Client> Clients { get; set; } = null!;
+
+        public DbSet<Truck> Trucks { get; set; } = null!;
+
+        public DbSet<Despatcher> Despatchers { get; set; } = null!;
+
+        public DbSet<Truck> ClientsTrucks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString)
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClientTruck>()
+                .HasKey(x => new { x.ClientId, x.TruckId });
+        }
+    }
+}
