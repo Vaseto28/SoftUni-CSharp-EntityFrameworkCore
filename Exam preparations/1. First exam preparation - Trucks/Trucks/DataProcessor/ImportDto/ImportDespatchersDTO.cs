425c1d098e3ac70e31776ea12cc476b640a144a0
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-24 10:31:17 +0300

    Completed task: Created ImportDespatchersDTO class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/DataProcessor/ImportDto/ImportDespatchersDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Trucks.Utilities;
+
+namespace Trucks.DataProcessor.ImportDto;
+
+[XmlType("Despatcher")]
+public class ImportDespatchersDTO
+{
+	[XmlElement("Name")]
+	[Required]
+	[MinLength(ValidationConstraints.DespatcherNameMinLength)]
+	[MaxLength(ValidationConstraints.DespatcherNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[XmlElement("Position")]
+	public string? Position { get; set; }
+
+	[XmlArray("Trucks")]
+	public ImportTrucksDTO[]? Trucks { get; set; }
+}
+
