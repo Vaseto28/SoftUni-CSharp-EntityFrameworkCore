3c19d3c6326209c57a087244c6acc7bd90463256
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-24 10:30:39 +0300

    Completed task: Created ExportTrucksDTO class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/DataProcessor/ExportDto/ExportTrucksDTO.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.DataProcessor.ExportDto;
+
+public class ExportTrucksDTO
+{
+	[JsonProperty("TruckRegistrationNumber")]
+	public string TruckRegistrationNumber { get; set; } = null!;
+
+	[JsonProperty("VinNumber")]
+	public string VinNumber { get; set; } = null!;
+
+	[JsonProperty("TankCapacity")]
+	public int TankCapacity { get; set; }
+
+	[JsonProperty("CargoCapacity")]
+	public int CargoCapacity { get; set; }
+
+	[JsonProperty("CategoryType")]
+	public string CategoryType { get; set; } = null!;
+
+	[JsonProperty("MakeType")]
+	public string MakeType { get; set; } = null!;
+}
+
