0a8225274ab26f5f0e53ea84819847245a912fe6
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-24 10:30:23 +0300

    Completed task: Created ExportDespatcherTrucksDTO class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/DataProcessor/ExportDto/ExportDespatcherTrucksDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto;
+
+[XmlType("Truck")]
+public class ExportDespatcherTrucksDTO
+{
+	[XmlElement("RegistrationNumber")]
+	public string RegistrationNumber { get; set; } = null!;
+
+	[XmlElement("Make")]
+	public string Make { get; set; } = null!;
+}
+
