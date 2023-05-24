916eeebc245098ac576cc8246e3baf826ccd6960
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-24 10:30:54 +0300

    Completed task: Created ImportClientsDTO class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/DataProcessor/ImportDto/ImportClientsDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Trucks.Utilities;
+
+namespace Trucks.DataProcessor.ImportDto;
+
+public class ImportClientsDTO
+{
+	[JsonProperty("Name")]
+	[Required]
+	[MinLength(ValidationConstraints.ClientNameMinLength)]
+	[MaxLength(ValidationConstraints.ClientNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[JsonProperty("Nationality")]
+	[Required]
+	[MinLength(ValidationConstraints.ClientNationalityMinLength)]
+	[MaxLength(ValidationConstraints.ClientNationalityMaxLength)]
+	public string Nationality { get; set; } = null!;
+
+	[JsonProperty("Type")]
+	[Required]
+	public string Type { get; set; } = null!;
+
+	[JsonProperty("Trucks")]
+	public int[] Trucks { get; set; } = null!;
+}
+
