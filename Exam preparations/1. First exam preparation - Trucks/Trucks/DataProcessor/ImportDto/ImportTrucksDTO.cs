fcd3bd935ca063480c338a5bf263992e248ba078
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-24 10:31:39 +0300

    Completed task: Created ImportTrucksDTO class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/DataProcessor/ImportDto/ImportTrucksDTO.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Trucks.Utilities;
+
+namespace Trucks.DataProcessor.ImportDto;
+
+[XmlType("Truck")]
+public class ImportTrucksDTO
+{
+	[XmlElement("RegistrationNumber")]
+	[Required]
+	[RegularExpression("[A-Z]{2}\\d{4}[A-Z]{2}")]
+	[MinLength(ValidationConstraints.TruckRegistrationNumberRequiredLength)]
+	[MaxLength(ValidationConstraints.TruckRegistrationNumberRequiredLength)]
+	public string RegistrationNumber { get; set; } = null!;
+
+	[XmlElement("VinNumber")]
+	[Required]
+    [MinLength(ValidationConstraints.TruckVinNumberRequiredLength)]
+    [MaxLength(ValidationConstraints.TruckVinNumberRequiredLength)]
+    public string VinNumber { get; set; } = null!;
+
+	[XmlElement("TankCapacity")]
+	[Range(ValidationConstraints.TruckTankCapacityMinValue, ValidationConstraints.TruckTankCapacityMaxValue)]
+	public int TankCapacity { get; set; }
+
+	[XmlElement("CargoCapacity")]
+    [Range(ValidationConstraints.TruckCargoCapacityMinValue, ValidationConstraints.TruckCargoCapacityMaxValue)]
+    public int CargoCapacity { get; set; }
+
+	[XmlElement("CategoryType")]
+	[Required]
+	public string CategoryType { get; set; } = null!;
+
+	[XmlElement("MakeType")]
+	[Required]
+	public string MakeType { get; set; } = null!;
+}
+
