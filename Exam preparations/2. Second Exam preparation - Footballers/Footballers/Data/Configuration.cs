219801d50296611d43ce9233d2ee0404343faa8b
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-28 13:42:41 +0300

    Completed task: Created Configuration class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Footballers.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=FootballersExam;User ID=sa;Password=28120601V;TrustServerCertificate=True;Trusted_Connection=True;Integrated security=false;";
+    }
+}
