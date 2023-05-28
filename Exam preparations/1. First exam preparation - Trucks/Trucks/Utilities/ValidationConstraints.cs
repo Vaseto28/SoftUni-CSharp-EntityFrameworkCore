c0ff7860a370f958f085744750cfbd260bbf65bc
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-28 13:42:01 +0300

    Completed task: Created ValidationConstraints class

---
+++ Exam preparations/1. First exam preparation - Trucks/Trucks/Utilities/ValidationConstraints.cs
@@ -0,0 +1,23 @@
+﻿namespace Trucks.Utilities;
+
+public static class ValidationConstraints
+{
+    //Truck
+    public const int TruckVinNumberRequiredLength = 17;
+    public const int TruckRegistrationNumberRequiredLength = 8;
+    public const int TruckTankCapacityMinValue = 950;
+    public const int TruckTankCapacityMaxValue = 1420;
+    public const int TruckCargoCapacityMinValue = 5000;
+    public const int TruckCargoCapacityMaxValue = 29000;
+
+    //Client
+    public const int ClientNameMinLength = 3;
+    public const int ClientNameMaxLength = 40;
+    public const int ClientNationalityMinLength = 2;
+    public const int ClientNationalityMaxLength = 40;
+
+    //Dispatcher
+    public const int DespatcherNameMinLength = 2;
+    public const int DespatcherNameMaxLength = 40;
+}
+
