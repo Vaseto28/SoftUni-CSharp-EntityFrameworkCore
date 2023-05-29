da213970503611b42f85cba605a6a2a59a85a353
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-29 08:12:11 +0300

    Completed task: Created ExportFootballersDTO class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/DataProcessor/ExportDto/ExportFootballersDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Footballers.DataProcessor.ExportDto;
+
+public class ExportFootballersDTO
+{
+	[JsonProperty("FootballerName")]
+	public string FootballerName { get; set; } = null!;
+
+	[JsonProperty("ContractStartDate")]
+	public string ContractStartDate { get; set; } = null!;
+
+	[JsonProperty("ContractEndDate")]
+	public string ContractEndDate { get; set; } = null!;
+
+	[JsonProperty("BestSkillType")]
+	public string BestSkillType { get; set; } = null!;
+
+	[JsonProperty("PositionType")]
+	public string PositionType { get; set; } = null!;
+}
+
