9efe5ec43cf5ef693e4c69974d241de8d55c0498
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-29 08:10:02 +0300

    Completed task: Created ExportCoachesDTO class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/DataProcessor/ExportDto/ExportCoachDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto;
+
+[XmlType("Coach")]
+public class ExportCoachDTO
+{
+	[XmlElement("CoachName")]
+	public string Name { get; set; } = null!;
+
+	[XmlArray("Footballers")]
+	public ExportFootballersByCoachesDTO[] Footballers { get; set; } = null!;
+
+	[XmlAttribute("FootballersCount")]
+	public int FootballersCount { get; set; }
+}
+
