1368aeb394df7385fd95dc6d1004c2f03a906614
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-30 09:01:06 +0300

    Completed task: Created ImportCoachesDTO class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/DataProcessor/ImportDto/ImportCoachesDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Footballers.Utilities;
+
+namespace Footballers.DataProcessor.ImportDto;
+
+[XmlType("Coach")]
+public class ImportCoachesDTO
+{
+	[XmlElement("Name")]
+	[Required]
+	[MinLength(ValidationConstraints.CoachNameMinLength)]
+	[MaxLength(ValidationConstraints.CoachNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[XmlElement("Nationality")]
+	[Required]
+	public string Nationality { get; set; } = null!;
+
+	[XmlArray("Footballers")]
+	public ImportFootballersDTO[] Footballers { get; set; } = null!;
+}
+
