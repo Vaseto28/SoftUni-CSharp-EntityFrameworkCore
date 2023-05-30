d540895bb6367e43624eddb9a98d0fe61d4109c3
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-30 09:02:06 +0300

    Completed task: Created ImportTeamsDTO class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/DataProcessor/ImportDto/ImportTeamsDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Footballers.Utilities;
+using Newtonsoft.Json;
+
+namespace Footballers.DataProcessor.ImportDto;
+
+public class ImportTeamsDTO
+{
+	[JsonProperty("Name")]
+	[Required]
+	[MinLength(ValidationConstraints.TeamNameMinLength)]
+	[MaxLength(ValidationConstraints.TeamNameMaxLength)]
+    [RegularExpression(ValidationConstraints.TeamNameRegex)]
+    public string Name { get; set; } = null!;
+
+	[JsonProperty("Nationality")]
+	[Required]
+	[MinLength(ValidationConstraints.TeamNationalityMinLength)]
+	[MaxLength(ValidationConstraints.TeamNationalityMaxLength)]
+	public string Nationality { get; set; } = null!;
+
+	[JsonProperty("Trophies")]
+	[Required]
+	public int Trophies { get; set; }
+
+	[JsonProperty("Footballers")]
+	[Required]
+	public int[] Footballers { get; set; } = null!;
+}
+
