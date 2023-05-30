25e8f12ac8a068c0770a95151160630ffae8bf6e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-05-30 09:01:42 +0300

    Completed task: Created ImportFootballersDTO class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/DataProcessor/ImportDto/ImportFootballersDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Footballers.Data.Models.Enums;
+using Footballers.Utilities;
+
+namespace Footballers.DataProcessor.ImportDto;
+
+[XmlType("Footballer")]
+public class ImportFootballersDTO
+{
+	[Required]
+	[MinLength(ValidationConstraints.FootballerNameMinLength)]
+	[MaxLength(ValidationConstraints.FootballerNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[XmlElement("ContractStartDate")]
+	[Required]
+	public string ContractStartDate { get; set; } = null!;
+
+	[XmlElement("ContractEndDate")]
+	[Required]
+	public string ContractEndDate { get; set; } = null!;
+
+	[XmlElement("BestSkillType")]
+	[Required]
+	[Range(0, 4)]
+	public int BestSkillType { get; set; }
+
+	[XmlElement("PositionType")]
+	[Required]
+	[Range(0, 3)]
+	public int PositionType { get; set; }
+}
+
