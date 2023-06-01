85239b0395648fac682fcff0ca754f6f77b5bac5
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-01 08:25:56 +0300

    Completed task: Created ValidationConstraints class

---
+++ Exam preparations/2. Second Exam preparation - Footballers/Footballers/Utilities/ValidationConstraints.cs
@@ -0,0 +1,20 @@
+﻿namespace Footballers.Utilities;
+
+public static class ValidationConstraints
+{
+	//Footballer
+	public const int FootballerNameMinLength = 2;
+	public const int FootballerNameMaxLength = 40;
+
+	//Team
+	public const int TeamNameMinLength = 3;
+	public const int TeamNameMaxLength = 40;
+	public const int TeamNationalityMinLength = 2;
+	public const int TeamNationalityMaxLength = 40;
+	public const string TeamNameRegex = "^[A-Za-z\\d\\s\\.\\-]{3,}$";
+
+    //Coach
+    public const int CoachNameMinLength = 2;
+	public const int CoachNameMaxLength = 40;
+}
+
