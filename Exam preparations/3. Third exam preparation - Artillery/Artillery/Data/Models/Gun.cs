ab97a8faf2a7042d07af6e0bca0f2ce3b0244b68
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-02 08:56:51 +0300

    Completed task: Created Gun model

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/Data/Models/Gun.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Artillery.Data.Models.Enums;
+
+namespace Artillery.Data.Models;
+
+public class Gun
+{
+	public Gun()
+	{
+		this.CountriesGuns = new HashSet<CountryGun>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	public int GunWeight { get; set; }
+
+	[Required]
+	public double BarrelLength { get; set; }
+
+	public int? NumberBuild { get; set; }
+
+	[Required]
+	public int Range { get; set; }
+
+	[Required]
+	public GunType GunType { get; set; }
+
+	[Required]
+	[ForeignKey(nameof(Manufacturer))]
+	public int ManufacturerId { get; set; }
+
+	public virtual Manufacturer Manufacturer { get; set; } = null!;
+
+	[Required]
+	[ForeignKey(nameof(Shell))]
+	public int ShellId { get; set; }
+
+	public virtual Shell Shell { get; set; } = null!;
+
+	public virtual ICollection<CountryGun> CountriesGuns { get; set; }
+}
+
