d913da2316d4a47b8777483c91c21a39ee48dcd7
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-02 08:56:09 +0300

    Completed task: Created Configuration class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Artillery.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=Artillery;User ID=sa;Password=28120601V;TrustServerCertificate=True;Integrated Security=false;Encrypt=False";
+    }
+}
