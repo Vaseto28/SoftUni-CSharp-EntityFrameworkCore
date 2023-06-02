4adc836cc31304c30d1b75b9d2d5bbbcd163ecd5
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-02 08:57:02 +0300

    Completed task: Created Manufacturer model

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Artillery.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Artillery.Data.Models;
+
+public class Manufacturer
+{
+	public Manufacturer()
+	{
+		this.Guns = new HashSet<Gun>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.ManufacturerNameMaxLength)]
+	public string ManufacturerName { get; set; } = null!;
+
+	[Required]
+	[MaxLength(ValidationConstraints.ManufacturerFoundedMaxLength)]
+	public string Founded { get; set; } = null!;
+
+	public virtual ICollection<Gun> Guns { get; set; }
+}
+
