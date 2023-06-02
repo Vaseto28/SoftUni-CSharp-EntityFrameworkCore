894ea0b09349fa6606fb71a5894d9e9d7a498c4e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-02 08:55:52 +0300

    Completed task: Created DbContext class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/Data/ArtilleryContext.cs
@@ -0,0 +1,46 @@
+﻿namespace Artillery.Data
+{
+    using Artillery.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    public class ArtilleryContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<Shell> Shells { get; set; }
+
+        public DbSet<Gun> Guns { get; set; }
+
+        public DbSet<CountryGun> CountriesGuns { get; set; }
+
+
+        public ArtilleryContext() 
+        {
+        }
+
+        public ArtilleryContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString)
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CountryGun>()
+                .HasKey(x => new { x.CountryId, x.GunId });
+        }
+    }
+}
