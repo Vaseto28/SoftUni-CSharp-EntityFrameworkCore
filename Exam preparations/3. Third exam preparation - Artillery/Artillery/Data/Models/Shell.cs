0ac3c15e9c55a94f8a000c7f8464a19dcb121ccb
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-02 08:57:12 +0300

    Completed task: Created Shell model

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/Data/Models/Shell.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Artillery.Common;
+
+namespace Artillery.Data.Models;
+
+public class Shell
+{
+	public Shell()
+	{
+		this.Guns = new HashSet<Gun>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	public double ShellWeight { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.ShellCaliberMaxLength)]
+	public string Caliber { get; set; } = null!;
+
+	public virtual ICollection<Gun> Guns { get; set; }
+}
+
