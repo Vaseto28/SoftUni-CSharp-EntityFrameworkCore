e374964d4dc6ba832f7ab5aeedfbb375269802d9
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-04 20:48:54 +0300

    Completed task: Created ImportGunsCountriesDTO class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ImportDto/ImportGunsCountriesDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+public class ImportGunsCountriesDTO
+{
+	[JsonProperty("Id")]
+	[Required]
+	public int Id { get; set; }
+}
+
