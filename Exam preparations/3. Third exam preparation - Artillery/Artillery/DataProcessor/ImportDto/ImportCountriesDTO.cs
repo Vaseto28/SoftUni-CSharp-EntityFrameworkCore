b70682e0f86594ba20918885538f8fd9c5719e26
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-04 20:48:38 +0300

    Completed task: Created ImportCountriesDTO class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ImportDto/ImportCountriesDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Artillery.Common;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+[XmlType("Country")]
+public class ImportCountriesDTO
+{
+	[XmlElement("CountryName")]
+	[Required]
+	[MinLength(ValidationConstraints.CountryNameMinLength)]
+	[MaxLength(ValidationConstraints.CountryNameMaxLength)]
+	public string CountryName { get; set; } = null!;
+
+	[XmlElement("ArmySize")]
+	[Required]
+	[Range(ValidationConstraints.CountryArmyMinSize, ValidationConstraints.CountryArmyMaxSize)]
+	public int ArmySize { get; set; }
+}
+
