8737d3f472ae408b2229c42adc216a8d9b874937
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-04 20:48:23 +0300

    Completed task: Created ExportShellsGunsDTO

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ExportDto/ExportShellsGunsDTO.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Artillery.DataProcessor.ExportDto;
+
+public class ExportShellsGunsDTO
+{
+	[JsonProperty("GunType")]
+	public string GunType { get; set; } = null!;
+
+	[JsonProperty("GunWeight")]
+	public int GunWeight { get; set; }
+
+	[JsonProperty("BarrelLength")]
+	public double BarrelLength { get; set; }
+
+	[JsonProperty("Range")]
+	public string Range { get; set; } = null!;
+}
+
