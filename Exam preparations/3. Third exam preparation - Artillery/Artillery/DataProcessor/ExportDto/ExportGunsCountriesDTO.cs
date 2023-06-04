f7d90863e14dab3cf6e9fcd7fb17c083c60c7ad8
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-04 20:47:33 +0300

    Completed task: Created ExportGunsCountriesDTO class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ExportDto/ExportGunsCountriesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ExportDto;
+
+[XmlType("Country")]
+public class ExportGunsCountriesDTO
+{
+	[XmlAttribute("Country")]
+	public string Country { get; set; } = null!;
+
+	[XmlAttribute("ArmySize")]
+	public int ArmySize { get; set; }
+}
+
