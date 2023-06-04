dae44a69900361a451c1f1f271a44bb73d1be9bf
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-04 20:49:17 +0300

    Completed task: Created ImportGunsDTO class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ImportDto/ImportGunsDTO.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Artillery.Common;
+using Newtonsoft.Json;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+public class ImportGunsDTO
+{
+	[JsonProperty("ManufacturerId")]
+	[Required]
+	public int ManufacturerId { get; set; }
+
+	[JsonProperty("GunWeight")]
+	[Required]
+	[Range(ValidationConstraints.GunWeightMinValue, ValidationConstraints.GunWeightMaxValue)]
+	public int GunWeight { get; set; }
+
+	[JsonProperty("BarrelLength")]
+	[Required]
+	[Range(ValidationConstraints.GunBarrelLengthMinValue, ValidationConstraints.GunBarrelLengthMaxValue)]
+	public double BarrelLength { get; set; }
+
+	[JsonProperty("NumberBuild")]
+	public int? NumberBuild { get; set; }
+
+	[JsonProperty("Range")]
+	[Required]
+	[Range(ValidationConstraints.GunRangeMinValue, ValidationConstraints.GunRangeMaxValue)]
+	public int Range { get; set; }
+
+	[JsonProperty("GunType")]
+	[Required]
+	public string GunType { get; set; } = null!;
+
+	[JsonProperty("ShellId")]
+	[Required]
+	public int ShellId { get; set; }
+
+	[JsonProperty("Countries")]
+	public ImportGunsCountriesDTO[]? Countries { get; set; }
+}
+
