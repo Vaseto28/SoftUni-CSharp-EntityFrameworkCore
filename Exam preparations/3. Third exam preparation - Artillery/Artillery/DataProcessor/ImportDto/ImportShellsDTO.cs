2154973334cb7d8a36d2451818b03590410a9f3f
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-05 22:37:21 +0300

    Completed task: Created ImportShells class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ImportDto/ImportShellsDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Artillery.Common;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+[XmlType("Shell")]
+public class ImportShellsDTO
+{
+	[XmlElement("ShellWeight")]
+	[Required]
+	[Range(ValidationConstraints.ShellWeightMinValue, ValidationConstraints.ShellWeightMaxValue)]
+	public double ShellWeight { get; set; }
+
+	[XmlElement("Caliber")]
+	[Required]
+	[MinLength(ValidationConstraints.ShellCaliberMinLength)]
+	[MaxLength(ValidationConstraints.ShellCaliberMaxLength)]
+	public string Caliber { get; set; } = null!;
+}
+
