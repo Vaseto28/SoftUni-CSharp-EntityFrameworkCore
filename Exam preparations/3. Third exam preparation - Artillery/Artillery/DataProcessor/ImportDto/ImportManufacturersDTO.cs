59d17af3fafeb08ece8e38ea928752527ced4489
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-05 22:37:05 +0300

    Completed task: Created ImportManufacturersDTO class

---
+++ Exam preparations/3. Third exam preparation - Artillery/Artillery/DataProcessor/ImportDto/ImportManufacturersDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Artillery.Common;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+[XmlType("Manufacturer")]
+public class ImportManufacturersDTO
+{
+	[XmlElement("ManufacturerName")]
+	[Required]
+	[MinLength(ValidationConstraints.ManufacturerNameMinLength)]
+	[MaxLength(ValidationConstraints.ManufacturerNameMaxLength)]
+	public string ManufacturerName { get; set; } = null!;
+
+	[XmlElement("Founded")]
+	[Required]
+	[MinLength(ValidationConstraints.ManufacturerFoundedMinLength)]
+	[MaxLength(ValidationConstraints.ManufacturerFoundedMaxLength)]
+	public string Founded { get; set; } = null!;
+}
+
