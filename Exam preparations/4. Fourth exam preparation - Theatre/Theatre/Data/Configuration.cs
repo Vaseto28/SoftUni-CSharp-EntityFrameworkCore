e290a209b5730e3d1b0def77d6e91dcc5e36265f
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-09 00:09:58 +0300

    Completed task: Created Configuration class

---
+++ Exam preparations/4. Fourth exam preparation - Theatre/Theatre/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Theatre.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=Theatre;User ID=sa;Password=28120601V;TrustServerCertificate=True;Integrated Security=False;Encrypt=False";
+    }
+}
