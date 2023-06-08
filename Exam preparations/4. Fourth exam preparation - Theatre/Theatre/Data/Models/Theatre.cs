ba7afcde82e2efcf521d91d69276dac2ad5a048d
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-09 00:10:30 +0300

    Completed task: Created Theathre model

---
+++ Exam preparations/4. Fourth exam preparation - Theatre/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Theatre.Common;
+
+namespace Theatre.Data.Models;
+
+public class Theatre
+{
+	public Theatre()
+	{
+		this.Tickets = new HashSet<Ticket>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.TheatreNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	public sbyte NumberOfHalls { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.TheatreDirectorMaxLength)]
+	public string Director { get; set; } = null!;
+
+	public virtual ICollection<Ticket> Tickets { get; set; }
+}
+
