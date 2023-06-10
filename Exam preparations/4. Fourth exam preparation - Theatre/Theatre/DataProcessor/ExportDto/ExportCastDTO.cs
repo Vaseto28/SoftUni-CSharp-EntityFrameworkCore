36c7eb65093dab28d77b212fa00a159178c0ad09
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-10 15:02:47 +0300

    Completed task: Created ExportCastDTO class

---
+++ Exam preparations/4. Fourth exam preparation - Theatre/Theatre/DataProcessor/ExportDto/ExportCastDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ExportDto;
+
+[XmlType("Actor")]
+public class ExportCastDTO
+{
+	[XmlAttribute("FullName")]
+	public string FullName { get; set; } = null!;
+
+	[XmlAttribute("MainCharacter")]
+	public string MainCharacter { get; set; } = null!;
+}
+
