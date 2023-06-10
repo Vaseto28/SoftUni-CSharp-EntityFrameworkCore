5dcfa2c5659637aa59d011b38d5623b6f3f7ef2e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-10 15:03:17 +0300

    Completed task: Created ExportTheatresDTO class

---
+++ Exam preparations/4. Fourth exam preparation - Theatre/Theatre/DataProcessor/ExportDto/ExportTheatresDTO.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Theatre.DataProcessor.ExportDto;
+
+public class ExportTheatresDTO
+{
+	[JsonProperty("Name")]
+	public string Name { get; set; } = null!;
+
+	[JsonProperty("Halls")]
+	public sbyte Halls { get; set; }
+
+	[JsonProperty("TotalIncome")]
+	public decimal TotalIncome { get; set; }
+
+	[JsonProperty("Tickets")]
+	public ExportTicketsDTO[] Tickets { get; set; } = null!;
+}
+
