0966b916d32fe455aa5923c587bdde9aac6ef5df
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-10 15:03:48 +0300

    Completed task: Created ImportCastsDTO class

---
+++ Exam preparations/4. Fourth exam preparation - Theatre/Theatre/DataProcessor/ImportDto/ImportCastsDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Common;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+[XmlType("Cast")]
+public class ImportCastsDTO
+{
+    [XmlElement("FullName")]
+    [Required]
+    [MinLength(ValidationConstraints.CastFullNameMinLength)]
+    [MaxLength(ValidationConstraints.CastFullNameMaxLength)]
+    public string FullName { get; set; } = null!;
+
+    [XmlElement("IsMainCharacter")]
+    [Required]
+    public bool IsMainCharacter { get; set; }
+
+    [XmlElement("PhoneNumber")]
+    [Required]
+    [RegularExpression(ValidationConstraints.CastPhoneNumberPattern)]
+    public string PhoneNumber { get; set; } = null!;
+
+    [XmlElement("PlayId")]
+    [Required]
+    public int PlayId { get; set; }
+}
+
