406da6244e87577e6fe13535d3157444f65b5801
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-11 21:37:28 +0300

    Completed task: Created ImportTicketsDTO class

---
+++ Exam preparations/4. Fourth exam preparation - Theatre/Theatre/DataProcessor/ImportDto/ImportTicketsDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Theatre.Common;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+public class ImportTicketsDTO
+{
+	[JsonProperty("Price")]
+	[Required]
+	[Range((double)ValidationConstraints.TicketPriceMinValue, (double)ValidationConstraints.TicketPriceMaxValue)]
+	public decimal Price { get; set; }
+
+	[JsonProperty("RowNumber")]
+	[Required]
+	[Range(ValidationConstraints.TicketRowNumberMinValue, ValidationConstraints.TicketRowNumberMaxValue)]
+	public sbyte RowNumber { get; set; }
+
+	[JsonProperty("PlayId")]
+	[Required]
+	public int PlayId { get; set; }
+}
+
