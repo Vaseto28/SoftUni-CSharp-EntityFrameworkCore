c19c27b05d9d86e41d1ac963dee2944c66d07c06
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-14 17:18:00 +0300

    Completed task: Created ValidationConstraints class

---
+++ Exam preparations/5. Fifth exam preparation - Tiester mask/TeisterMask/Common/ValidationConstraints.cs
@@ -0,0 +1,19 @@
+﻿namespace TeisterMask.Common;
+
+public static class ValidationConstraints
+{
+	//Employee
+	public const int EmployeeUsernameMinLength = 3;
+	public const int EmployeeUsernameMaxLength = 40;
+	public const string EmployeeUsernameRegExPattern = "^[A-Za-z\\d]{3,}$";
+	public const string EmployeePhoneRegExPattern = "^[\\d]{3}\\-[\\d]{3}\\-[\\d]{4}$";
+
+	//Project
+	public const int ProjectNameMinLength = 2;
+	public const int ProjectNameMaxLength = 40;
+
+	//Task
+	public const int TaskNameMinLength = 2;
+	public const int TaskNameMaxLength = 40;
+}
+
