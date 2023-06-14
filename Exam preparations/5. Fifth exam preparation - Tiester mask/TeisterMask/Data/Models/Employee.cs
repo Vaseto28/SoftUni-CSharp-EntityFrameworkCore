d9e17cc1be8a690e26215773a0a50ebc390abe46
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-14 17:18:28 +0300

    Completed task: Created Employee model

---
+++ Exam preparations/5. Fifth exam preparation - Tiester mask/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TeisterMask.Common;
+
+namespace TeisterMask.Data.Models;
+
+public class Employee
+{
+	public Employee()
+	{
+		this.EmployeesTasks = new HashSet<EmployeeTask>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.EmployeeUsernameMaxLength)]
+	public string Username { get; set; } = null!;
+
+	[Required]
+	public string Email { get; set; } = null!;
+
+	[Required]
+	public string Phone { get; set; } = null!;
+
+	public virtual ICollection<EmployeeTask> EmployeesTasks { get; set; }
+}
+
