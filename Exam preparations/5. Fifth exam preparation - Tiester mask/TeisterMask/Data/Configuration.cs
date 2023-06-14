3841bcd24d43276130247510a852a13224aa35d9
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-14 17:18:16 +0300

    Completed task: Created Configuration class

---
+++ Exam preparations/5. Fifth exam preparation - Tiester mask/TeisterMask/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=.;Database=TeisterMaskExam;User ID=sa;Password=28120601V;TrustServerCertificate=True;Integrated Security=False;Encrypt=False";
+    }
+}
