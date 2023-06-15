07cbffdedac7b1c34aabbe56e4f06bdb83043a25
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-15 11:09:34 +0300

    Completed task: Created Project model

---
+++ Exam preparations/5. Fifth exam preparation - Tiester mask/TeisterMask/Data/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TeisterMask.Common;
+
+namespace TeisterMask.Data.Models;
+
+public class Project
+{
+	public Project()
+	{
+		this.Tasks = new HashSet<Task>();
+	}
+
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(ValidationConstraints.ProjectNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	public DateTime OpenDate { get; set; }
+
+	public DateTime? DueDate { get; set; }
+
+	public virtual ICollection<Task> Tasks { get; set; }
+}
+
