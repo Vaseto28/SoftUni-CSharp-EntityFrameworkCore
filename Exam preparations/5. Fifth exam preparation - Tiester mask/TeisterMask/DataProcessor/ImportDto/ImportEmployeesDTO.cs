c5882121adc92b52278886b0301bd55c8bb029e1
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-15 11:10:30 +0300

    Completed task: Created ImportEmployeesDTO class

---
+++ Exam preparations/5. Fifth exam preparation - Tiester mask/TeisterMask/DataProcessor/ImportDto/ImportEmployeesDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using TeisterMask.Common;
+
+namespace TeisterMask.DataProcessor.ImportDto;
+
+public class ImportEmployeesDTO
+{
+	[JsonProperty("Username")]
+	[Required]
+	[MinLength(ValidationConstraints.EmployeeUsernameMinLength)]
+	[MaxLength(ValidationConstraints.EmployeeUsernameMaxLength)]
+	[RegularExpression(ValidationConstraints.EmployeeUsernameRegExPattern)]
+	public string Username { get; set; } = null!;
+
+	[JsonProperty("Email")]
+	[Required]
+	[EmailAddress]
+	public string Email { get; set; } = null!;
+
+	[JsonProperty("Phone")]
+	[Required]
+	[RegularExpression(ValidationConstraints.EmployeePhoneRegExPattern)]
+	public string PhoneNumber { get; set; } = null!;
+
+	[JsonProperty("Tasks")]
+	public int[]? Tasks { get; set; }
+}
+
