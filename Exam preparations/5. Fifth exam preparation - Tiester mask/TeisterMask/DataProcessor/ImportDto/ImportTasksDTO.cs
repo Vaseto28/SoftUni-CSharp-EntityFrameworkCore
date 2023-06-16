f1ff85d4539c67f91329c91ffab14c05b81d8ddf
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-06-16 18:07:14 +0300

    Completed task: Created ImportTasksDTO class

---
+++ Exam preparations/5. Fifth exam preparation - Tiester mask/TeisterMask/DataProcessor/ImportDto/ImportTasksDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using TeisterMask.Common;
+
+namespace TeisterMask.DataProcessor.ImportDto;
+
+[XmlType("Task")]
+public class ImportTasksDTO
+{
+	[XmlElement("Name")]
+	[Required]
+	[MinLength(ValidationConstraints.TaskNameMinLength)]
+	[MaxLength(ValidationConstraints.TaskNameMaxLength)]
+	public string Name { get; set; } = null!;
+
+	[XmlElement("OpenDate")]
+	[Required]
+	public string OpenDate { get; set; } = null!;
+
+	[XmlElement("DueDate")]
+	[Required]
+	public string DueDate { get; set; } = null!;
+
+	[XmlElement("ExecutionType")]
+	[Required]
+	[Range(0 ,3)]
+	public int ExecutionType { get; set; }
+
+	[XmlElement("LabelType")]
+	[Required]
+	[Range(0, 4)]
+	public int LabelType { get; set; }
+}
+
